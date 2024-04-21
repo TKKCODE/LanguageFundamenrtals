57ababaa2ef910fc857826efcf56d7c08ae3984a
TKKCODE <kirankumarthommandru@gmail.com>
2024-04-21 12:56:06 +0530

    Add project files.

---
+++ LanguageFundamenrtals.cpp
@@ -0,0 +1,16 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a, b;
+    cin >> a;
+    cout << "treaty" << endl;
+    cin >> b;
+
+    cout << a + b << " .. " << endl;;
+}
+
+
